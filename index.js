6987bc2521c3d9541efc8303b8cd5aae171eba9b
Amos Koech <koechronix@yahoo.com>
2022-06-03 10:35:04 -0400

    completed

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+console.log(cats);
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+    return cats;
+}
+
+console.log(destructivelyAppendCat("cat"));
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+    return cats;
+
+}
+console.log(destructivelyPrependCat("cat"));
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+    return cats;
+}
+console.log(destructivelyPrependCat("cat"))
+
+function destructivelyRemoveFirstCat(){
+    cats.pop();
+    return cats;
+}
+console.log(destructivelyAppendCat("cat"))
